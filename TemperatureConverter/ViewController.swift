736e08fbc8eb7d61528790a940f5b36b82656931
Heaven Wesker <Heaven.Wesker@gmail.com>
2014-11-14 01:58:30 +0800

    About auto layout

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var CLabel: UILabel!
     @IBOutlet weak var FLabel: UILabel!
     @IBOutlet weak var KLabel: UILabel!
+    @IBOutlet weak var TitleLabel: UILabel!
     @IBOutlet weak var tempSlider: UISlider!
     @IBAction func temperatureSliderChanged(sender: UISlider) {
         self.updateTemperatureDisplays()
@@ -31,9 +32,22 @@ class ViewController: UIViewController {
         var cTemperature = tempSlider.value
         CLabel.text = String(format: "%.1fC", arguments:[cTemperature])
         FLabel.text = String(format: "%.1fF", arguments:[cTemperature*9/5+32])
-        KLabel.text = String(format: "%.1fK", arguments:[cTemperature+273.15])
+        KLabel.text = String(format: "%.2fK", arguments:[cTemperature+273.15])
     }
-
-
+    
+//    override func viewWillLayoutSubviews() {
+//        super.viewWillLayoutSubviews()
+//        let screen = UIScreen.mainScreen()
+//        //Always return the size of the screen in portraits orientation
+//        println("Screen fix bounds \(screen.fixedCoordinateSpace.bounds)")
+//        //Since ios8, this is the size of the screen in its current orientation
+//        println("Screen bounds \(screen.bounds)")
+//        println("Top layout guide: \(topLayoutGuide.length)")
+//        //println(toString(TitleLabel.frame.width))
+//        //TitleLabel.frame.width = screen.bounds.width-16
+//        //TitleLabel.frame.width = 100
+//        TitleLabel.frame = CGRect(x: 16.0, y: topLayoutGuide.length+16.0,
+//            width: screen.bounds.width-32, height: 21)
+//    }
 }
 
