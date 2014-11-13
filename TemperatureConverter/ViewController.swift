a7955780c70e184b3e1c85373febc7b7f7d0e9da
Heaven Wesker <Heaven.Wesker@gmail.com>
2014-11-13 18:51:51 +0800

    Layout for iPhone 6 iOS 8.1

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CLabel: UILabel!
+    @IBOutlet weak var FLabel: UILabel!
+    @IBOutlet weak var KLabel: UILabel!
+    @IBOutlet weak var tempSlider: UISlider!
+    @IBAction func temperatureSliderChanged(sender: UISlider) {
+        self.updateTemperatureDisplays()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +25,13 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func updateTemperatureDisplays(){
+        var cTemperature = tempSlider.value
+        CLabel.text = String(format: "%.1fC", arguments:[cTemperature])
+        FLabel.text = String(format: "%.1fF", arguments:[cTemperature*9/5+32])
+        KLabel.text = String(format: "%.1fK", arguments:[cTemperature+273.15])
     }
 
 
